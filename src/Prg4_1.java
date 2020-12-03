73c487d9141366513c3bb70aaf83d4f8fb4c3c9d
HWQhe <941822900@qq.com>
2020-12-03 09:45:56 +0800

    updated

---
+++ src/Prg4_1.java
@@ -0,0 +1,12 @@
+/*
+Prg4-1 CSMA/CD
+ Write a program to simulate the behavior of the CSMA/CD protocol over Ethernet when there are
+ N stations ready to transmit while a frame is being transmitted. Your program should report the
+ times when each station successfully starts sending its frame. Assume that a clock tick occurs
+ once every slot time (51.2 usec) and a collision detection and sending of a jamming sequence
+ takes one slot time. All frames are the maximum length allowed.
+ */
+public class Prg4_1
+{
+
+}
---
+++ src/Prg5_1.java
@@ -0,0 +1,16 @@
+/*
+Prg5-1 flooding
+ Write a program to simulate routing using flooding. Each packet should contain a counter that is
+ decremented on each hop. When the counter gets to zero, the packet is discarded. Time is
+ discrete, with each line handling one packet per time interval. Make three versions of the
+ program: all lines are flooded, all lines except the input line are flooded, and only the
+ (statically chosen) best k lines are flooded. Compare flooding with deterministic routing
+ ( k = 1 ) in terms of both delay and the bandwidth used.
+ */
+public class Prg5_1
+{
+    public static void init()
+    {
+
+    }
+}
